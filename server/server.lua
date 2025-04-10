d64234106ee3377aaa03fd3f3d297808dc2d6427
tvasa <t.vasa@centrum.cz>
2025-04-10 17:50:36 +0200

    aaa

--- server/server.lua
+++ server/server.lua
@@ -1,13 +1,2 @@
 local RSGCore = exports['rsg-core']:GetCoreObject()
 
-local function selectRandomLocation(locations)
-    local index = math.random(#locations)
-    return locations[index]
-end
-
-local npcLocation = selectRandomLocation(Config.NPC.zones)
-
-RegisterNetEvent('test_script:server:loadNpc')
-AddEventHandler('test_script:server:loadNpc', function()
-    TriggerClientEvent('test_script:client:loadNpc', source, npcLocation)
-end)
